8220cd6e1eb73af82f8a20436581ff2373404e99
Sufi <sufrus_2004@hotmail.com>
2026-09-04 17:17:03 +0800

    Fourth Post

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ print("My first post!")
 print("=====")
 
 username = input("Enter Username: ")
-age = input("Enter age: ")
+age = int(input("Enter age: "))
 category = input("Enter Content Category: ")
 
 print("\Instagram Profile")
@@ -12,3 +12,6 @@ print("=====")
 print("Username:", username)
 print("Age:", age)
 print("Category:", category)
+
+if age>40 and category == "fun":
+    print("You are old what is fun for you??")
