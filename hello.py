b257cffd9098e9ed51f5c96e4ea8cdaf3dc87d23
Sufi <sufrus_2004@hotmail.com>
2026-09-04 17:12:10 +0800

    Third Post

--- hello.py
+++ hello.py
@@ -3,19 +3,12 @@ print("Welcome here")
 print("My first post!")
 print("=====")
 
-username = "cool_creator"
-bio = "Fun Blogger"
-followers = 100
+username = input("Enter Username: ")
+age = input("Enter age: ")
+category = input("Enter Content Category: ")
 
+print("\Instagram Profile")
+print("=====")
 print("Username:", username)
-print("Bio", bio)
-print("Followers", followers)
-
-followers += 50
-print("Day 1", followers)
-
-followers += 20
-print("Day 2:", followers)
-
-followers -= 10
-print("Day 3:", followers)
+print("Age:", age)
+print("Category:", category)
